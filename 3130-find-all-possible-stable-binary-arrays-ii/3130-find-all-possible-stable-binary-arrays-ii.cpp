9976d6246fbb22ed9b97d7859d3223223259a803
Radhika <radheya981@gmail.com>
2026-03-10 20:05:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
+++ 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
     int numberOfStableArrays(int zero, int one, int limit) {
-        int mod = 1e9 + 7;
-        vector<vector<vector<int>>> memo(
-            zero + 1, vector<vector<int>>(one + 1, vector<int>(2, -1)));
+        const int MOD = 1e9 + 7;
+        vector<vector<array<long,2>>> dp(
+            zero+1, vector<array<long,2>>(one+1, {0LL,0LL}));
 
-        function<int(int, int, int)> dp = [&](int zero, int one,
-                                              int lastBit) -> int {
-            if (zero == 0) {
-                return (lastBit == 0 || one > limit) ? 0 : 1;
-            } else if (one == 0) {
-                return (lastBit == 1 || zero > limit) ? 0 : 1;
+        for (int i = 1; i <= min(zero,limit); i++) dp[i][0][0] = 1;
+        for (int j = 1; j <= min(one, limit); j++) dp[0][j][1] = 1;
+
+        for (int i = 1; i <= zero; i++) {
+            for (int j = 1; j <= one; j++) {
+                long over0 = (i-limit >= 1) ? dp[i-limit-1][j][1] : 0;
+                long over1 = (j-limit >= 1) ? dp[i][j-limit-1][0] : 0;
+                dp[i][j][0] = (dp[i-1][j][0] + dp[i-1][j][1] - over0 + MOD) % MOD;
+                dp[i][j][1] = (dp[i][j-1][0] + dp[i][j-1][1] - over1 + MOD) % MOD;
             }
-
-            if (memo[zero][one][lastBit] == -1) {
-                int res = 0;
-                if (lastBit == 0) {
-                    res = (dp(zero - 1, one, 0) + dp(zero - 1, one, 1)) % mod;
-                    if (zero > limit) {
-                        res = (res - dp(zero - limit - 1, one, 1) + mod) % mod;
-                    }
-                } else {
-                    res = (dp(zero, one - 1, 0) + dp(zero, one - 1, 1)) % mod;
-                    if (one > limit) {
-                        res = (res - dp(zero, one - limit - 1, 0) + mod) % mod;
-                    }
-                }
-                memo[zero][one][lastBit] = res % mod;
-            }
-            return memo[zero][one][lastBit];
-        };
-
-        return (dp(zero, one, 0) + dp(zero, one, 1)) % mod;
+        }
+        return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
     }
 };
